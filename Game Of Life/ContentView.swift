c492e84ca1fd782da8fdfe7c9102f8cb73c931b8
uwi0 <kakapoweb@gmail.com>
2024-09-26 19:45:51 +0700

    Initial Commit

---
+++ Game Of Life/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Game Of Life
+//
+//  Created by dwi prasetyo on 26/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Game Of Life/Game_Of_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Of_LifeApp.swift
+//  Game Of Life
+//
+//  Created by dwi prasetyo on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_Of_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
