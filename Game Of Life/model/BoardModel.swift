c0aaf74ddc3a998d0150e43036a7501163aa2b25
uwi0 <kakapoweb@gmail.com>
2024-09-27 10:27:54 +0700

    creating model for game and give it default value

---
+++ Game Of Life/model/BoardModel.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+struct BoardModel {
+    var numRow: Int
+    var numColumn: Int
+    var board: [Int]
+    var surviveRules: [Bool]
+    var bordRules: [Bool]
+    
+    let defaultSurviveRules: [Bool] = [
+        false, false, true, true, false, false,
+        false,false,false
+    ]
+    
+    let defaultBornRules: [Bool] = [
+        false, false, false, true, false, false,
+        false,false,false
+    ]
+    let MAX_CREATURE_AGE: Int = 10
+    
+    init(numRow: Int, numColumn: Int) {
+        self.numRow = numRow
+        self.numColumn = numColumn
+        self.board = Array(repeating: 1, count: numRow * numColumn)
+        self.surviveRules = defaultSurviveRules
+        self.bordRules = defaultBornRules
+        self.randomBoard()
+    }
+    
+    mutating func randomBoard() {
+        for i in 0..<board.count {
+            let randomElement = Int.random(in: 0..<2)
+            board[i] = randomElement
+        }
+    }
+    
+    static func deafultBoard(numRows: Int, numCol: Int) -> BoardModel {
+        return BoardModel(numRow: numRows, numColumn: numCol)
+    }
+}
