01d8c3577b81e70cdc13add30b8adeffa28d5842
uwi0 <kakapoweb@gmail.com>
2024-09-27 10:55:17 +0700

    creating board view

---
+++ Game Of Life/view/support/BoardView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct BoardView: View {
+    @Binding var board: BoardModel
+    @Binding var isGridShowing: Bool
+    
+    private var numRow: Int { board.numRow }
+    private var numColumn: Int { board.numColumn }
+    private var gridItemLayout: [GridItem] {
+        Array(repeating: GridItem(.flexible(), spacing: 0), count: numRow)
+    }
+    private var borderColor: Color {
+        isGridShowing ? .blue : .clear
+    }
+    
+    var body: some View {
+        LazyVGrid(columns: gridItemLayout, spacing: 0) {
+            let creature = board.getCreature(i: 0)
+            let vitality = board.vitatlity(creature: 0)
+            let isDead = creature == 0
+            let creatureColor = creatureColor(isDead, vitality)
+            
+            Rectangle()
+                .fill(creatureColor)
+                .border(borderColor)
+                .overlay {
+                    Rectangle()
+                        .stroke(.pink.opacity(0.7), lineWidth: 1)
+                }
+            
+        }
+    }
+    
+    private func creatureColor(_ isDead: Bool, _ vitality: Double) -> Color {
+        isDead ? .black : Color(red: 1, green: vitality, blue: vitality)
+    }
+}
+
+#Preview {
+    BoardView(
+        board: .constant(.deafultBoard(numRows: 10, numCol: 10)),
+        isGridShowing: .constant(false)
+    )
+}
